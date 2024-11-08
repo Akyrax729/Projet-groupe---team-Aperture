0ee194c6df583b321d462a56e3c63cdee520e756
Soufiane El Alami <soufianeelalami1991@gmail.com>
2024-11-08 14:49:24 +0100

    5eme push

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const title = document.getElementById("signalement-title");
+ function blinkTitle() {       
+                    title.style.visibility = (title.style.visibility === 'hidden') ? 'visible' : 'hidden';
+                }
+    setInterval(blinkTitle, 500);
+    
